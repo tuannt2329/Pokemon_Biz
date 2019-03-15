a32747b2a2ae2e2c0d145ec730d1a015e5742536
nguyen anh tuan <nguyenanhtuan29091997@gmail.com>
2019-03-15 16:56:52 +0700

    update Load map

--- Pokemon_Biz/my-app/src/index.js
+++ Pokemon_Biz/my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import LoadMap from "./components/loadmap";
+import LoadMap from "./components/loadmap"
 
 ReactDOM.render(<LoadMap />, document.getElementById('root'));
+
